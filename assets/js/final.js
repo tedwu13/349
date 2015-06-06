55db7a5e1d64c966c9fa56f7b48e2bf5c670b2fc
Ted Wu <tedwu2016@u.northwestern.edu>
2015-06-06 17:05:57 -0500

    scroll top

--- assets/js/final.js
+++ assets/js/final.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
 
 
-
     $('.objective').click(function(){
-        $('html, body').animate(scrollTop:$('#objective').position().top}, 'slow');
+        $('html, body').animate({scrollTop:($('#objective').position().top)-100}, 'slow');
         return false;
     });
 
 
-    $('.portfolioa').click(function(){
-        $('html, body').animate(scrollTop:$('#objective').position().top}, 'slow');
+    $('.data_collection').click(function(){
+        $('html, body').animate({scrollTop:($('#data_collection').position().top)-100}, 'slow');
         return false;
     });
-    $('.contacta').click(function(){
-        $('html, body').animate(scrollTop:$('#objective').position().top}, 'slow');
+    $('.design_decisions').click(function(){
+        $('html, body').animate({scrollTop:($('#design_decisions').position().top)-100}, 'slow');
         return false;
     });
+
+    $('.results').click(function(){
+        $('html, body').animate({scrollTop:($('#results').position().top)-100}, 'slow');
+        return false;
+    });
+
+    $('.next_steps').click(function(){
+        $('html, body').animate({scrollTop:($('#next_steps').position().top)-100}, 'slow');
+        return false;
+    });
+
 });
