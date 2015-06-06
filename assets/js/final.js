2a401df93f6f4e32e1cac0b45bae370c3916c7c7
Ted Wu <tedwu2016@u.northwestern.edu>
2015-06-06 16:32:32 -0500

    added jquery scrolling function

---
+++ assets/js/final.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+
+    $('.objective').click(function(){
+        $('html, body').animate(scrollTop:$('#objective').position().top}, 'slow');
+        return false;
+    });
+
+
+    $('.portfolioa').click(function(){
+        $('html, body').animate(scrollTop:$('#objective').position().top}, 'slow');
+        return false;
+    });
+    $('.contacta').click(function(){
+        $('html, body').animate(scrollTop:$('#objective').position().top}, 'slow');
+        return false;
+    });
+});
