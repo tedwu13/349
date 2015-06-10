e09fabe90b805f4ebfb610159da59eb699c592b2
Ted Wu <tedwu2016@u.northwestern.edu>
2015-06-10 15:15:04 -0500

    dj3 js visualization

--- assets/js/final.js
+++ assets/js/final.js
@@ -1,28 +1,23 @@
 $(document).ready(function() {
 
 
-    $('.objective').click(function(){
-        $('html, body').animate({scrollTop:($('#objective').position().top)-100}, 'slow');
+    $('.problem').click(function(){
+        $('html, body').animate({scrollTop:($('#problem').position().top)-100}, 'slow');
         return false;
     });
 
 
-    $('.data_collection').click(function(){
-        $('html, body').animate({scrollTop:($('#data_collection').position().top)-100}, 'slow');
+    $('.solution').click(function(){
+        $('html, body').animate({scrollTop:($('#solution').position().top)-100}, 'slow');
         return false;
     });
-    $('.design_decisions').click(function(){
-        $('html, body').animate({scrollTop:($('#design_decisions').position().top)-100}, 'slow');
+    $('.methods').click(function(){
+        $('html, body').animate({scrollTop:($('#methods').position().top)-100}, 'slow');
         return false;
     });
 
     $('.results').click(function(){
         $('html, body').animate({scrollTop:($('#results').position().top)-100}, 'slow');
-        return false;
-    });
-
-    $('.next_steps').click(function(){
-        $('html, body').animate({scrollTop:($('#next_steps').position().top)-100}, 'slow');
         return false;
     });
 
